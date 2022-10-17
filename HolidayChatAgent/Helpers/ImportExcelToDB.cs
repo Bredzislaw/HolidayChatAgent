abafaea9a9d51de2916aaf7aa54f23e5ca137efb
limak1 <48149920+limak1@users.noreply.github.com>
2022-10-17 16:44:24 +0100

    Excel converter

---
+++ HolidayChatAgent/Helpers/ImportExcelToDB.cs
@@ -0,0 +1,44 @@
+﻿using ExcelDataReader;
+using HolidayChatAgent.Models;
+using HolidayChatAgent.Properties;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidayChatAgent.Helpers
+{
+    internal class ImportExcelToDB
+    {
+        public void ImportExcel() 
+        {
+            DataTableCollection tableCollection;
+
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Excel 97-2003 Workbook|*.xls|Excel Workbook|*.xlsx" })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    var dataSource = "C:/Users/Kamil/source/repos/HolidayChatAgent/HolidayChatAgent/Resources/";
+                    using (var stream = File.Open(ofd.FileName, FileMode.Open, FileAccess.Read))
+                    {
+                        using (IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream))
+                        {
+                            DataSet result = reader.AsDataSet(new ExcelDataSetConfiguration()
+                            {
+                                ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+                                {
+                                    UseHeaderRow = true
+                                }
+                            });
+                            tableCollection = result.Tables;                          
+                           // foreach (DataTable table in tableCollection)
+                              //  cboSheet.Items.Add(table.TableName);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ HolidayChatAgent/Models/Countries.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidayChatAgent.Models
+{
+    internal class Countries
+    {
+        public string HolidayReference { get; set; }
+        public string HotelName { get; set; }
+        public string City { get; set; }
+        public string Continent { get; set; }
+        public string Country { get; set; }
+        public string Category { get; set; }
+        public int StarRating { get; set; }
+        public string TempRating { get; set; }
+        public string Location { get; set; }
+        public string PricePerNight { get; set; }
+
+
+
+    }
+}
